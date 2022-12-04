688b265c53accc4be8a0c7f57a7c0c1f31bc6e6a
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2022-12-04 18:50:46 +0100

    updates

---
+++ main.js
@@ -0,0 +1,53 @@
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false,
+    transitionStyle:"fade",
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1      
+         },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false,
+    transitionStyle:"fade",
+    items:1,
+    singleItem: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2     
+         },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$(".hamburger").click(function(){
+    $(".hamburgerCLICK").toggle("slow");
+  });
+$(".foto").click(function(){
+    var src=document.getElementById('foto1').src;
+    var img = document.createElement('img');
+    img.src=src;
+    document.getElementById('body').appendChild = img;
+});
